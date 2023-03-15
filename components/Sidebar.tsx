15c8f65bd57d4651d1106ddefadb5a093131dfea
Cerqueira <contatocerq@gmail.com>
2023-03-14 23:53:36 -0300

    done

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {BsBellFill, BsHouseFill} from 'react-icons/bs'
 import {FaUser} from 'react-icons/fa'
+import SidebarLogo from './SidebarLogo'
 
 const Sidebar = () => {
   const items = [
@@ -22,8 +23,10 @@ const Sidebar = () => {
   ]
   return (
     <div className='col-span-1 h-full pr-4 md:pr-6'>
-      <div className='flex flex-col items-end'>
-        <div className='space-y-2 lg:w-[230]'></div>
+      <div className='flex flex-col items-start'>
+        <div className='space-y-2 lg:w-[230]'>
+          <SidebarLogo />
+        </div>
       </div>
     </div>
   )
