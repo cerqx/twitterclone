159b697b097b57e2201a44a05a69317cf47fcbbe
Cerqueira <contatocerq@gmail.com>
2023-03-14 23:39:14 -0300

    Sidebar beginning

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import {BsBellFill, BsHouseFill} from 'react-icons/bs'
+import {FaUser} from 'react-icons/fa'
 
 const Sidebar = () => {
+  const items = [
+    {
+      label: 'Home',
+      href: '/',
+      icon: BsHouseFill
+    },
+    {
+      label: 'Notifications',
+      href: '/notifications',
+      icon: BsBellFill
+    },
+    {
+      label: 'Profile',
+      href: '/users/123',
+      icon: FaUser
+    },
+  ]
   return (
-    <div>Sidebar</div>
+    <div></div>
   )
 }
 
