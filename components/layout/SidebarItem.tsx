326685eae980a152ed35dc5ba42f4c657c733655
Cerqueira <contatocerq@gmail.com>
2023-03-15 23:32:03 -0300

    Done

--- components/layout/SidebarItem.tsx
+++ components/layout/SidebarItem.tsx
@@ -1,4 +1,12 @@
 import React from 'react'
+import { IconType } from 'react-icons';
+
+interface SidebarItemProps {
+  label: string;
+  href: string;
+  icon: IconType;
+  onClick?: () => void;
+}
 
 const SidebarItem = () => {
   return (
