cbffc67deeb863fc431be393a664b5e9596bff41
Cerqueira <contatocerq@gmail.com>
2023-03-16 23:33:53 -0300

    Done

--- components/Header.tsx
+++ components/Header.tsx
@@ -21,9 +21,17 @@ const Header: React.FC<HeaderProps> = ({label, showBackArrow}) => {
           showBackArrow && (
             <BiArrowBack 
               onClick={handleBack}
+              color="white"
+              size={20}
+              className="
+                cursor-pointer
+                hover:opacity-70
+                transition
+              "
             />
           )
         }
+        <h1 className='text-white text-xl font-semibold'>{label}</h1>
       </div>
     </div>
   )
