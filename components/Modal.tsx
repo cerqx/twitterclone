e271084a87c1fdbd99cba4912077a80b5923db16
Cerqueira <contatocerq@gmail.com>
2023-03-18 20:19:20 -0300

    Done

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,5 +1,6 @@
 import React, { useCallback } from 'react'
 import { AiOutlineClose } from 'react-icons/ai'
+import Button from './Button';
 
 interface ModalProps {
   isOpen: boolean;
@@ -119,6 +120,10 @@ const Modal: React.FC<ModalProps> = ({
             {/* Body */}
             <div className='relative p-10 flex-auto'>
               {body}
+            </div>
+            {/* Footer */}
+            <div className='flex flex-col gap-2 p-10'>
+              <Button disabled={disabled} label={actionLabel} secondary fullWidth large onClick={handleSubmit}/>
             </div>
           </div>
         </div>
