4fa861c550f943b85823ad3051185bbd007158a8
Cerqueira <contatocerq@gmail.com>
2023-03-17 23:30:01 -0300

    done

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -102,7 +102,7 @@ const Modal: React.FC<ModalProps> = ({
             >
               <h3 className='text-3xl font-semibold text-white'>{title}</h3>
               <button>
-                <AiOutlineClose />
+                <AiOutlineClose size={24}/>
               </button>
             </div>
           </div>
