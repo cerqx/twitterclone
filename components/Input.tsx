21cc3561de8128302ed1f2785d68f0949da7d86b
Cerqueira <contatocerq@gmail.com>
2023-03-19 12:52:12 -0300

    Done

--- components/Input.tsx
+++ components/Input.tsx
@@ -16,7 +16,30 @@ const Input: React.FC<InputProps> = ({
   onChange
 }) => {
   return (
-    <div>Input</div>
+    <input 
+      disabled={disabled}
+      type={type}
+      value={value}
+      placeholder={placeholder}
+      onChange={onChange}
+      className='
+        w-full
+        p-4
+        text-lg
+        bg-black 
+        border-2
+        border-neutral-800
+        rounded-md
+        outline-none
+        text-white
+        focus:border-sky-500
+        focus:border-2
+        transition
+        disabled:bg-neutral-900
+        disabled:opacity-70
+        disabled:cursor-not-allowed
+      '
+    />
   )
 }
 
