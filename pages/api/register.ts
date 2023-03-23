769b454033cd533142044ef1b573c47bc7112bbc
Cerqueira <contatocerq@gmail.com>
2023-03-23 20:33:47 -0300

    Register

--- pages/api/register.ts
+++ pages/api/register.ts
@@ -1,4 +1,7 @@
+import bcrypt from 'bcrypt';
 import { NextApiRequest, NextApiResponse } from "next";
+
+import prisma from '@/libs/prismadb';
 
 export default async function handler(
   request: NextApiRequest, 
@@ -10,6 +13,19 @@ export default async function handler(
 
     try {
       const { email, username, name, password} = request.body;
+
+      const hashedPassword = await bcrypt.hash(password, 12);
+
+      const user = await prisma.user.create({
+        data: {
+          email,
+          username,
+          name,
+          hashedPassword
+        }
+      });
+
+      return response.status(200).json(user);
     } catch (error) {
       console.log(error)
       return response.status(400).end();
