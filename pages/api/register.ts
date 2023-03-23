dc3c232a9e3b9893f5fed54d59e9cdcfde3ac0e9
Cerqueira <contatocerq@gmail.com>
2023-03-23 20:29:38 -0300

    Done

--- pages/api/register.ts
+++ pages/api/register.ts
@@ -4,5 +4,14 @@ export default async function handler(
   request: NextApiRequest, 
   response: NextApiResponse 
 ) {
+    if(request.method !== 'POST'){
+      return response.status(405).end();
+    }
 
+    try {
+      const { email, username, name, password} = request.body;
+    } catch (error) {
+      console.log(error)
+      return response.status(400).end();
+    }
   }
