f263647503423d74559ff01ffb0f018064c7ca8a
Cerqueira <contatocerq@gmail.com>
2023-03-27 22:32:49 -0300

    done

--- components/modals/LoginModal.tsx
+++ components/modals/LoginModal.tsx
@@ -1,5 +1,6 @@
 import useLoginModal from '@/hooks/useLoginModal'
 import useRegisterModal from '@/hooks/useRegisterModal';
+import { signIn } from 'next-auth/react';
 import React, { useCallback, useState } from 'react'
 import Input from '../Input';
 import Modal from '../Modal';
@@ -25,7 +26,10 @@ const LoginModal = () => {
     try {
       setIsLoading(true);
 
-      //TODO ADD LOG IN
+      await signIn('credentials', {
+        email,
+        password
+      })
 
       loginModal.onClose();
     } catch(err) {
@@ -33,7 +37,7 @@ const LoginModal = () => {
     } finally {
       setIsLoading(false);
     }
-  }, [loginModal])
+  }, [loginModal, email, password])
 
   const bodyContent = (
     <div className='flex flex-col gap-4'>
@@ -46,6 +50,7 @@ const LoginModal = () => {
 
       <Input 
         placeholder='Password'
+        type='password'
         value={password}
         onChange={(event) => setPassword(event.target.value)}
         disabled={isLoading}
